89aaf3f7d0be9bd8e0b57ec352d7393b9d3dcffa
philip dutra <monsterphilip10@gmail.com>
2023-08-20 14:34:25 -0400

    updated lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,35 @@
 from models import Dog
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (create_engine, desc,
+    Index, Column, DateTime, Integer, String)
+from sqlalchemy.orm import sessionmaker
 
-def create_table(base):
-    pass
+
+def create_table(base, engine):
+    if __name__ == '__main__':
+        base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    dogs = session.query(Dog).all()
+    return dogs
 
 def find_by_name(session, name):
-    pass
+    dog = session.query(Dog).filter(Dog.name.like(f'{name}')).first()
+    return dog
 
 def find_by_id(session, id):
-    pass
+    dog = session.query(Dog).filter(Dog.id == id).first()
+    return dog
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    dog = session.query(Dog).filter(Dog.name.like(f'{name}'), Dog.breed.like(f'{breed}')).first()
+    return dog
 
 def update_breed(session, dog, breed):
-    pass
+    session.query(Dog).filter(Dog.id == dog.id).update({
+        Dog.breed: breed
+    })
